d12e892bc9d2fa8c4d3092dd8caa7756383eb95d
Md Javed Akhtar <mdjavedakhtar7323@gmail.com>
2024-04-02 14:31:46 +0530

    index4 is updated

--- javascript/index4.js
+++ javascript/index4.js
@@ -1,1 +1,1 @@
-learning github from coding sarathi youtube channel and this text is added through github website manually not using git bash
+learning github from coding sarathi youtube channel and this text is added through github website manually not using git bash and this text is added through git bash
